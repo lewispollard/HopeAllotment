b98d0f793baadaa8e46cd769d48e8529b141651e
Lewis Pollard <lewiji@gmail.com>
2020-10-17 15:20:58 +0100

    chore: add map marker, fix alignment

--- src/pages/location.js
+++ src/pages/location.js
@@ -12,10 +12,37 @@ const LocationPage = () => {
         <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400 uppercase">
           Location
         </h2>
-        <p>Gimme dat google map</p>
-        <MapContainer
-          selectedPlace={{ lat: 53.41631155973524, lng: -1.4416388707955667 }}
-        />
+        <section className="md:w-2/3 mx-auto text-justify px-4 space-y-3">
+          <p>
+            We rent 4 plots on the{" "}
+            <a
+              href="http://wikimapia.org/16817069/High-Wincobank-allotments"
+              rel="noreferrer"
+              target="_blank"
+              className="text-purple-600 font-semibold hover:text-purple-500"
+            >
+              High Wincobank Allotment Site
+            </a>
+            . The site entrance is opposite house no 343 Windmill lane. The plot
+            entrance is a few doors up the path on the right hand side on plot
+            80.{" "}
+          </p>
+
+          <p>
+            We are but a short walk away from Firth park centre which is
+            accessible via the bus routes:
+          </p>
+          <ul className="mx-auto text-center space-y-2">
+            <li>First: 1A, 3, 35, 38, 75, 76</li>
+            <li>Stagecoach: 88</li>
+            <li>TM Travel: 17, 32</li>
+          </ul>
+        </section>
+        <section className="w-2/3 mx-auto mt-6">
+          <MapContainer
+            selectedPlace={{ lat: 53.41561155973524, lng: -1.442938707955667 }}
+          />
+        </section>
       </main>
     </Layout>
   );
