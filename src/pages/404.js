e678c25c82cedd606e4bb9b8ffee31061466acec
Lewis Pollard <lewiji@gmail.com>
2020-10-18 17:17:46 +0100

    fix: misaligned heading on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,13 +7,13 @@ function NotFoundPage() {
   return (
     <div>
       <SEO title="404: Not found" />
-      <div>
+      <div className="text-center">
         <img
           alt="Ghost getting abducted by aliens"
           className="block mx-auto w-1/2"
           src={abductionIllustration}
         />
-        <h2 className="headingpattern text-2xl font-bold inline-block my-8 p-3">
+        <h2 className="headingpattern text-2xl font-bold mx-auto inline-block my-8 p-3">
           Looks like this page was not found...
         </h2>
       </div>
